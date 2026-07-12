04a4392267624883a3f97123190eee81c9555cf3
QuachVanTu <quachvantuzzz@gmail.com>
2026-07-12 13:17:58 +0700

    Implement tower rotation toward target

---
+++ Assets/Scripts/Tower/TowerRotation.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TowerRotation : MonoBehaviour
+{
+    [SerializeField] private Transform rotationTransform;
+    private TowerTargeting towerTargeting;
+    private void Awake()
+    {
+        towerTargeting = GetComponent<TowerTargeting>();
+    }
+    private void Update()
+    {
+        Enemy target = towerTargeting.GetCurrentTarget();
+        if (target == null)
+        {
+            return;
+        }
+        Vector3 positionWeapon = rotationTransform.position;
+        Vector3 positionEnemy = target.transform.position;
+        Vector3 direction = (positionEnemy - positionWeapon);
+        Quaternion quaternion = Quaternion.LookRotation(direction);
+        float rotationSpeed = 10f;
+        rotationTransform.rotation = Quaternion.Lerp(rotationTransform.rotation, quaternion, Time.deltaTime * rotationSpeed);
+    }
+}
